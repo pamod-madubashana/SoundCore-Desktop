9a4d8a41ed15a000de884e915d1dcda558522efe
pamod-madubashana <pamod.main@gmail.com>
2026-09-07 04:13:04 +0530

    chore(site): migrate site entry point to TypeScript

--- .site/src/main.tsx
+++ .site/src/main.tsx
@@ -4,10 +4,10 @@ import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </StrictMode>
+  </StrictMode>,
 );
